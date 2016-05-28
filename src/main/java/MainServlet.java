35b710a90728c3526d0f5f9d609ed9cdaf068fb4
nsmalimov <norman000@mail.ru>
2016-05-28 16:20:40 +0400

    Update MainServlet.java

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -13,24 +13,10 @@ public class MainServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-//        Cookie cookie = null;
-//        Cookie[] cookies = null;
-//        cookies = request.getCookies();
-
-        //if (cookies != null) {
             response.setContentType("text/html");
             RequestDispatcher dispatcherChat = request.getRequestDispatcher("chat.html");
             if (dispatcherChat != null) {
                 dispatcherChat.forward(request, response);
             }
-        //}
-        //куки небыли присланы
-//        else {
-//            response.setContentType("text/html");
-//            RequestDispatcher dispatcherAuto = request.getRequestDispatcher("chat.html");
-//            if (dispatcherAuto != null) {
-//                dispatcherAuto.forward(request, response);
-//            }
-//        }
     }
 }
